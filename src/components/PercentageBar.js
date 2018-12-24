674126cdd0a71cd9b6d15b18c7d675c39288de2e
Judy <yanghuanghuang@gmail.com>
2018-12-23 22:45:20 -0800

    make component PercentageBar functional component

--- src/components/PercentageBar.js
+++ src/components/PercentageBar.js
@@ -1,18 +1,14 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-class PercentageBar extends Component {
-  render () {
-    return (
-      <div className='percentage-bar'>
-        <div
-          className='percentage-bar-progress'
-          style={{ width: this.props.percentage + '%' }}
-        >
-          {this.props.percentage}%
-        </div>
-      </div>
-    )
-  }
-}
+const PercentageBar = props => (
+  <div className='percentage-bar'>
+    <div
+      className='percentage-bar-progress'
+      style={{ width: props.percentage + '%' }}
+    >
+      {props.percentage}%
+    </div>
+  </div>
+)
 
 export default PercentageBar
